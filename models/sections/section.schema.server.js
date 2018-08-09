e0db95087af7ad3595a782ce3d59460ceb386873
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-09 01:17:08 -0400

    schema created for sections

--- models/sections/section.schema.server.js
+++ models/sections/section.schema.server.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.Schema ({
     title: String,
-    courseId : String
+    courseId : String,
+    maxSeats: Number,
+    remSeats:Number,
 },{collection:'section'});
