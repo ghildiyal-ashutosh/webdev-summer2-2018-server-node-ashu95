29e6f4d1dafa80ebea7035058936feaac5aee92c
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-09 01:14:22 -0400

    user service modified

--- services/user.service.server.js
+++ services/user.service.server.js
@@ -23,11 +23,48 @@ module.exports = app => {
     }
 
     logOut = (req,res) => {
-        let user = {username:'', password:''};
+        let user = {username:'Negative', password:''};
         req.session['currentUser'] = user;
         res.send(req.session);
 
     }
+
+    registerUser = (req,res) =>
+        userModel.createUser(req.body)
+            .then(function (user) {
+                req.session['currentUser'] = user;
+                req.send(user);
+
+            })
+
+
+    findUserByUsername = (req,res) =>
+        userModel.findUserByCredentials(req.body)
+            .then(user => res.json(user));
+
+    updateProfile = (req,res) =>
+
+        userModel.updateUser(req.body)
+            .then (function(){
+                req.session['currentUser'] = user;
+                res.send(user);
+            })
+
+    function deleteProfile (req,res) {
+        req.session.destroy();
+        res.send(200);
+    }
+
+
+
+
+    findUserById = (req,res) =>
+        userModel.findUserById(req.params.userId)
+            .then(user => res.json(user));
+
+    findProfile  = (req,res) =>
+        res.send(req.session['currentUser']);
+
 
         app.get('/currentUser', currentUser);
         app.get('/api/user', findAllUsers);
