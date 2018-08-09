4ce8a85a5484c8b10b1fc76e1828d2cfa131610f
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-09 01:17:42 -0400

    section models modified

--- models/sections/section.model.server.js
+++ models/sections/section.model.server.js
@@ -4,16 +4,63 @@ const sectionSchema = require ('./section.schema.server');
 
 const sectionModel = mongoose.model('SectionModel', sectionSchema);
 
+const userModel = require('../user/user.model.server');
+
 createSection = section =>
     sectionModel.create(section);
-
-
 
 findSectionForCourse  = (courseId) =>
     sectionModel.find({courseId:courseId})
 
+findAllSections = () =>
+    sectionModel.find();
+
+
+findSectionById = (sectionId) =>
+    sectionModel.find({_id : sectionId });
+
+decreaseSectionSeats = (sectionId) =>
+    sectionModel.update ({
+        _id: sectionId
+    }, {
+        $inc: {remSeats: -1}
+    });
+
+increaseSectionSeats = (sectionId) =>
+    sectionModel.update({
+        _id: sectionId
+        }, {
+        $inc: {remSeats : +1}
+    });
+
+updateSection = (section) =>
+    sectionModel.update({
+        _id: section.sectionId
+    },{
+        title: section.title,
+        maxSeats : section.maxSeats,
+        remSeats : section.remSeats,
+
+
+    });
+
+ deleteSection = (sectionId) =>
+   sectionModel.deleteOne({_id: sectionId})
+
+
+
+
+
+
 
 module.exports = {
     createSection,
-    findSectionForCourse
+    findSectionForCourse,
+    findAllSections,
+    findSectionById,
+    updateSection,
+    increaseSectionSeats,
+    decreaseSectionSeats,
+    deleteSection
+
 };
