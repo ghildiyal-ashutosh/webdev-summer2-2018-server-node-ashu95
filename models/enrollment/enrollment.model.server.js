4984c36fbf18cf32918cd59f771d82ce86f5bf39
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-09 01:13:07 -0400

    enrollment model created

--- models/enrollment/enrollment.model.server.js
+++ models/enrollment/enrollment.model.server.js
@@ -2,4 +2,29 @@ const mongoose = require('mongoose');
 
 const enrollmentSchema = require('./enrollment.schema.server');
 
-var enrollmentModel = mongoose.model("EnrollmentModel", enrollmentSchema)
+const enrollmentModel = mongoose.model("EnrollmentModel", enrollmentSchema)
+
+
+findSectionsForStudent = (studentId) =>
+    enrollmentModel
+         .find({student : studentId})
+         .populate ('section')
+         .exec();
+
+enrollStudentSection = (enrollment) =>
+    enrollmentModel.create(enrollment);
+
+unenrollStudentSection = (enrollment) =>
+    enrollmentModel.deleteOne(enrollment);
+
+deleteEnrollmentSection = (enrollment) =>
+    enrollmentModel.remove(enrollment);
+
+
+module.exports = {
+    findSectionsForStudent,
+    enrollStudentSection,
+    unenrollStudentSection,
+    deleteEnrollmentSection
+
+};
