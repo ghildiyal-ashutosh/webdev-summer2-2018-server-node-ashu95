d2629b3ade6437fa96da83b86dcc1dbb4101049e
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-10 04:49:33 -0400

    db added

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://heroku_9kz4wcwb:mg5iq7qrj3igaff8vpgo7eolr3@ds119052.mlab.com:19052/heroku_9kz4wcwb')
+
+
 // cross origin call (CORS)
 app.use(function (req,res,next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin" , 'https://webdev2-angular-ashu95.herokuapp.com');
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -19,11 +24,16 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(session({
     resave: false,
     saveUninitialized : true,
-    secret: 'any string'
-}));
+    secret: 'any string',
+    cookie: {
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/webdev2_summer2018');
+        maxAge: 30 * 60 * 1000
+    },
+    rolling: true
+}
+));
+
+
 
 require('./services/user.service.server')(app);
 require('./services/section.service.server')(app);
