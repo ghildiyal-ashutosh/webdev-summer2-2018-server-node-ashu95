a9ac544b9c89dcc799a48fca105671d3999fde7f
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-13 03:26:25 -0400

    Changes in question fields

--- models/question/question.schema.server.js
+++ models/question/question.schema.server.js
@@ -4,6 +4,12 @@ module.exports = mongoose.Schema ({
     title: String,
     points: Number,
     description: String,
+    questionText: String,
+    choices : [{
+        text: String,
+        value: String,
+        correct: Boolean
+    }],
     questionType: {type: String,
                    enum: ['ESSAY','FILL_BLANKS','TRUE_FALSE', 'MULTIPLE_CHOICE']}
 }, {collection: 'question'});
